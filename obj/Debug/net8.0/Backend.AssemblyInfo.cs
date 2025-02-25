2f245603ffb0a2dd50d20e18d35a403a4e967fd1
KSaiVishnu <svt20042005@gmail.com>
2025-02-25 19:40:05 +0530

    Changes in AppSettings

--- obj/Debug/net8.0/Backend.AssemblyInfo.cs
+++ obj/Debug/net8.0/Backend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+29ad42dbc8b8398d188216549a429fa9b40f3035")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+79805e13cef5a3cd8cb2392c63ce32b57354d960")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
